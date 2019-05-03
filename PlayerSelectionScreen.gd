bf4ea7dfdae1626f9177ac1bfaab8430b3ab95ff
david1platt <david1platt@gmail.com>
2019-05-02 20:00:49 -0700

    added character select screen that just goes to the test stage

---
+++ PlayerSelectionScreen.gd
@@ -0,0 +1,45 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Dave_pressed():
+	get_tree().change_scene("Main.tscn") # Replace with function body.
+
+
+func _on_Miah_pressed():
+	get_tree().change_scene("Main.tscn")
+
+
+func _on_John_pressed():
+	get_tree().change_scene("Main.tscn")
+
+
+func _on_Merlin_pressed():
+	get_tree().change_scene("Main.tscn")
+
+
+func _on_Mr_T_pressed():
+	get_tree().change_scene("Main.tscn")
+
+
+func _on_Ben_pressed():
+	get_tree().change_scene("Main.tscn")
+
+
+func _on_Rodney_pressed():
+	get_tree().change_scene("Main.tscn")
+
+
+func _on_Burns_pressed():
+	get_tree().change_scene("Main.tscn")
